ebbe9ccfc0548b77d391bda128bdb9d54059967a
tanishk050504 <Tsharma0219@conestogac.on.ca>
2023-02-07 20:43:40 -0800

    Subtract function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,7 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-
+void sub();
 
 void main() {
 
@@ -21,6 +21,10 @@ void main() {
 	{
 	case 1:
 		add();
+		break;
+	case 2:
+		sub();
+		break;
 	}
 
 }
@@ -46,5 +50,17 @@ void add() {
 	printf("Enter the third value:");
 	scanf_s("%lf", &num3);
 	result = num1 + num2+num3;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	printf("%lf + %lf = %lf\n", num1, num2, num3, result);
 }
+void sub() {
+	double num1, num2, num3, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	printf("Enter the third value:");
+	scanf_s("%lf", &num3);
+	result = num1 - num2 - num3;
+	printf("%lf - %lf - %lf = %lf\n", num1, num2, num3, result);
+}
+
